0bf462fc6af4ce6147704f4873e1e6a620072db1
russmaier <russmaier@gmail.com>
2022-09-27 15:19:55 +0800

    php?

---
+++ en/test.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="http://earthcup.io/stylesheet.css?v2.1">
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" > 
+
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
+</noscript>
+
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet" media="print" onload="this.media='all'"/>
+
+<link rel="shortcut icon" href="https://earthcup.io/favicon.ico" type="image/x-icon">
+<link rel="icon" href="https://earthcup.io/favicon.ico" type="image/x-icon">
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, target-densitydpi=160">
+<meta name="apple-mobile-web-app-title" content="EarthCup">
+
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+<link rel="apple-touch-icon" sizes="57x57" href="https://earthcup.io/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://earthcup.io/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://earthcup.io/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://earthcup.io/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://earthcup.io/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://earthcup.io/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://earthcup.io/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://earthcup.io/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://earthcup.io/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://earthcup.io/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://earthcup.io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://earthcup.io/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://earthcup.io/favicon-16x16.png">
+
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="https://earthcup.io/icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<link rel="shortcut icon" href="https://earthcup.io/icons/favicon.ico" type="image/x-icon">
+<link rel="icon" href="https://earthcup.io/icons/favicon.ico" type="image/x-icon">
+
+
+
+
+<title>EarthCup | The cup that follows the Earth's example</title>
+<meta name="keywords" content="tumbler, take away cup, coffee cup, cup, cups, organic cup, tea cup, coffee tumbler, thermos, bamboo cup, bamboo to-go cup, earth cup, plastic solution, green tumbler, flask, beverage holder">
+<meta name="description" content="The Greenest Coffee Cup Ever ">
+
+  <meta property="og:url"           content="https://www.earthcup.io">
+  <meta property="og:type"          content="website">
+ <meta property="og:title"         content="EarthCup | The Green Cup Subscription">
+  <meta property="og:description"   content="The EarthCup is a beautiful, functional, resusable, hand-crafted cup that removes CO2 and plastic from the biosphere." >
+  <meta property="og:image"         content="https://earthcup.io/images/fb-banner.png" >
+
+</meta>
+
+</head>
+
+
+<BODY id="full-page">
+  <div id="header" class="top-menu">
+    <a href="index.html"><img src="/svgs/earthcup-logo.svg?v1.5" width="202" height="" alt="EarthCup Logo"></a>
+
+    <div class="topnav-lang" id="myTopnav-lang">
+        <div class="dropdown-lang">
+        <button class="dropbtn-lang"><img src="/svgs/language-button2.svg" height="30px"></button>
+        <div class="dropdown-content-language">
+          <a href="/en/">🇬🇧&nbsp;EN</a>
+          <a href="/id/">🇮🇩&nbsp;ID</a> 
+          <a href="/es/">🇪🇸&nbsp;ES</a>
+          <a href="/fr/">🇫🇷&nbsp;FR</a>
+      
+          <a href="javascript:void(0);" style="font-size:15px;" class="icon-lang" onclick="languageMenu()"></a>	
+        </div>
+      </div> 
+    </div>
+  </div>
+
+  <div class="flex-photo">
+    <img src="/photos/banner-2022-1600px.jpg" width="100%">
+  </div>
+
+<div id="feature-photos">
+  <div class="flex-photo">
+    <img src="/photos/beverage-2022-1000px.jpg" width="100%">
+  </div>
+
+  <div class="flex-photo">
+    <img src="/photos/planted-2022-1000px.jpg" width="100%">
+  </div>
+
+  <div class="flex-photo">
+    <img src="/photos/pedestal-750px.jpg" width="100%">
+  </div>
+
+  <div class="flex-photo">
+    <img src="/photos/making-750px.jpeg" width="100%">
+  </div>
+
+</div>
+
+<div id="main">
+<!--<p><img src="/images/earthcup-2-1-logo.svg" width="110px;" alt="EarthCup Logo"></p>-->
+<br><br>
+<h1>The Greenest Cup Ever.</h1> 
+
+<h2>The EarthCup is a beautiful, functional, reusable take-away cup hand-crafted from bamboo and teak wood.  Not only does it carry your favorite beverage in epic green style— it removes CO2 and plastic from the biosphere.</h2><br>
+
+<div id="image"><img src="/images/virtue-icons.svg?v=3.1" width="80%" alt="Regenerative Virtue Icons"></div>
+<p>Moving on from petro-plastics and industrial production, the EarthCup is the ultimate green drinking cup.  Made for-Earth, rather than for-profit, made by hand rather than by factory, made from bamboo and wood rather than industrial materials, the EarthCup takes green to a whole new level.</p>
+<p>Unlike normal disposal cups and reusable tumblers whose manufacture results in plastic being made and carbon being emitted, the generation of an EarthCup results in just the opposite.  By sequestering carbon in its biomass and taking the place of polluting alternatives, the EarthCup is a net-green regenerative product.</p> 
+
+  
+<br>
+<div class="chart"><img src="/svgs/co2impact.svg?v=3.1" alt="CO2 Impact Chart"></div>
+<div class="chart"><img src="/svgs/plastic-impact.svg?v=3.1" alt="CO2 Impact Chart"></div>
+<p>See the full ecological accounting report:</p>
+<div id="image"><a href="/pdfs/EarthCup-Product-Impact-Assessment-Report.pdf" target="_blank"><img src="/images/impact-report-button-700px.png" width="350px" alt="Regenerative Product Impact Report"></a></div>
+
+<br>
+
+<p><b>Also unlike many cups, the EarthCup uses no leaching plastic liner.  Relying on bamboo's naturally waterproof interior we use NO inside sealants or additives of any kind.</b></p>
+
+
+
+<p>EarthCups are now available for purchase on the Gobrik Regen store for <b>15$ USD</b> + shipping.</p>
+<div class="chart"><div class="w3-card-4"><a href="https://www.gobrik.com/regen-store.php#shop/review-product/5ef04ccb39466500158386ef/" target="_blank"><img src="/images/regenstore-full-750px.png" alt="Regen Store Purchase Link"></a></div></div>
+
+<h6>Yearly Cup Subscriptions coming soon!</h6><br><br>
+ 
+
+<!--
+<h6>The EarthCup is another Regenerative Innovation by Russell Maier.<br>Copyright 2022 <a href="www.russs.net">Russs.net</a></h6>
+<br>
+<a href="https://www.russs.net"><img src="images/R-icon-150px.png" width="70px";></a><br><br>-->
+
+</div>
+
+<?php require_once ("/footers/en.php");?>
+<!--FOOTER STARTS HERE-->
+	
+<div id="footer-full">
+
+  <div id="vision-landscape"><img src="/svgs/bamboo-footer.svg?v=1" width="100%" height="63%"; position="absolute"; bottom="0"; ></div>
+		
+	
+	<div class="footer-vision">We raise our glass to a world where every drink enriches the ecosystems and biomes of which we are a part.</div>
+	
+	<!--
+	<div class="footer-invite">Connect with us on our various platforms and servers.</div>-->
+	</div>
+
+	
+	
+	<div class="footer-content">
+
+		<div class="footer-box">
+			<div class="footer-header">For-Earth</div>
+				<div class="footer-subtext">EarthCups.io is a not-for-profit, Earth enterprise.  We direct all our revenues towards our for-Earth intention.  We account for both our financial revenues and expenses and our ecological give and take.<br><br>
+				</div>
+				
+			</div>
+			<div class="footer-box">
+				<div class="footer-header">Ecological Accounting</div>
+				<div class="footer-subtext">In order to be sure that the EarthCups are in fact net-green, we keep track of our carbon, plastic and biodiversity impacts.  We use a <a href="https://ecobricks.org/catalyst" target="_blank">GEA Catalyst account</a> to do this and we publish our annual Regenerativity Reports each year.</a><br><br></div>
+				<!--<div class="footer-header">Trainer Support</div>-->
+				
+			</div>
+			<div class="footer-box">
+				<div class="footer-header">Earthen Ethics</div>
+				<div class="footer-subtext">EarthCups are a net-green regenerative product that embody the principles of <a href="https://earthen.io/imagine">Earthen Ethics</a>.  Our product is circular, for-Earth, subtractive, biodiversity supporting and eco-awaring.</div>
+			</div>
+			<div class="footer-box">
+
+			<div class="footer-header">Buy</div>
+				<div class="footer-subtext">EarthCups are available for purchase on the GoBrik Regen store for fiat-currency and brikcoins.  <a href="https://www.gobrik.com/regen-store.php" target="_blank">Get yours today!</a></div>
+
+				</div>
+			</div>
+		</div>
+				
+	<div class="footer-bottom">
+	
+
+	<div class="footer-conclusion">	
+		<div class="row">
+
+          <div class="virtue">
+              <img src="/banners/forearth-black-500px.webp" width="325px" height="" alt="We operate as a not-for-profit, for-Earth enterprise" title="For-Earth" loading="lazy"></a>
+          </div>
+
+          <div class="virtue">
+            <a href="https://www.gobrik.com/#my-catalyst/enterprise-disclosure/5b8c2b0922ddfb2591bf67be/" target="_blank"><img src="/banners/disclose-black-500px.webp" width="325px" height="" alt="Proudly Net-Green: We track, disclose and accelerate our plastic transition. View our annual Regnen Report." title="Click through to see a our annual Regen Report" loading="lazy"></a>
+          </div>
+        
+
+         <!-- <div class="virtue">
+            <a href="https://www.websitecarbon.com/website/earthcup-io/" target="_blank"><img src="banners/co2-black-500px.webp" width="325px" height="" alt="Click through to see the live CO2 accounting for our site." title="Click through to see our live carbon accounting." loading="lazy"></a>
+          </div>-->
+
+          <div class="virtue">
+            <div id="carbon-banner">
+            <div id="wcb" class="carbonbadge wcb-d"></div>
+            <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
+			    </div></div>
+
+		</div>		
+	</div>
+
+		
+
+<br>
+		<div class="footer-invite">
+			This 100% hand-coded open-source page is hosted on Github.<br>📝 <a href="https://github.com/russs95/earthcup" target="_blank" rel="noopener">Propose a change to this page</a>
+		</div>
+
+
+		<br><br>
+	</div>
+</div>
+
+<!-- ARC DELAY LOAD SCRIPT  This is a special script to load the arc script 8 seconds after the full page has loaded, this prevents Arc from slowing down our page load and boost our google page speed score
+                -->
+
+                <script type="text/javascript" defer>
+
+                  document.addEventListener("DOMContentLoaded", function(event) {
+                    setTimeout(addScript, 8000)
+                    });
+                    
+                    function addScript() {
+                    
+                    script = document.createElement('script');
+                    script.type = 'text/javascript';
+                    script.async = true;
+                    script.onload = function() {
+                      console.log("Added Script");
+                    };
+                    script.src = 'https://arc.io/widget.min.js#mJFtQsHF';
+                    document.getElementsByTagName('head')[0].appendChild(script);
+                    }
+                  
+                  
+                  </script>
+                  
+                   <!--Arc CDN Script without delay
+                  <script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>-->
+	<!--FOOTER ENDS HERE-->
+
+	
+
+</div>
+</body>
+</html>
+
+
+
